97cf33944887866f90bc05f77617a44df9882ae7
eptaccio <eptaccio@gmail.com>
2017-12-07 23:09:10 -0200

    feat: update app

---
+++ sw.js
@@ -0,0 +1,9 @@
+'use strict'
+
+self.addEventListener('install', function (event) {
+  console.log('install event')
+})
+
+self.addEventListener('activate', function (event) {
+  console.log('activate event')
+})
